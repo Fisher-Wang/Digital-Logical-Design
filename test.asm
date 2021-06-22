2895ef4137410ddd6d8f0a4d1377eda3c996a303
Fisher-Wang <kingwfsh@163.com>
2021-06-22 16:33:57 +0800

    can add and display result correctly

--- test.asm
+++ test.asm
@@ -5,6 +5,7 @@ addi x6, x0, 0 # operator
 addi x15, x0, 0x7f
 slli x15, x15, 8
 addi x15, x15, 0x20
+addi x16, x0, 0 # inputed
 
 ## first num and op
 L1:
@@ -18,6 +19,7 @@ beq x11, x8, L2
 
 addi x5, x7, 0
 addi x6, x8, 0
+addi x14, x7, 0
 
 ## LOOP
 LOOP:
@@ -26,7 +28,15 @@ LOOP:
 L11:
 lw x8, 0x7f00
 lw x7, 0x7f10
+
+beq x7, x14, IF1_END
+addi x16, x0, 1
+IF1_END:
+
+beq x16, x0, IF2_END
 sw x7, (x15)
+IF2_END:
+
 beq x8, x0, L11
 L22:
 lw x11, 0x7f00
@@ -40,7 +50,12 @@ L4B:
 add x5, x5, x7
 L4E:
 
+# display result
+sw x5, (x15)
+
 # ready for next loop
+addi x16, x0, 0
+lw x14, 0x7f10
 addi x6, x8, 0
 
 j LOOP
--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 int x0, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14;
 int a, b, c;
+int inputed = 0;
 const int ADD = 0, SUB = 1;
 void func()
 {
@@ -18,13 +19,18 @@ void func()
 
     x5 = x7;
     x6 = x8;
-
+    x14 = x7; // record portb
 
     for (;;) {
         /* another num and op */
         do {
             x8 = a;
             x7 = b;
+            // display last output until new num was input
+            if (x7 != x14)
+                inputed = 1;
+            if (inputed != 0)
+                c = x7;
         } while (x8 == 0);
         do {
             x11 = a;
@@ -34,7 +40,10 @@ void func()
             x5 = x5 + x7;
         else if (x6 == SUB)
             x5 = x5 - x7;
+        c = x5; // display output
         /* ready for next loop */
+        inputed = 0; // reset inputed
+        x14 = b; // record portb
         x6 = x8;
     }
 }
