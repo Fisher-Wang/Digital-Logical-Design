4ad0fb39400fc6aee12047d95b3be3647dc12c25
Fisher-Wang <kingwfsh@163.com>
2021-06-23 12:08:17 +0800

    good div2

--- test/div.asm
+++ test/div.asm
@@ -1,10 +1,10 @@
 # divisor x7
 # reminder x5
-# quotient x20
+# quotient x24
 # counter x22
 li x5 17
 li x7 5
-li x20 0
+li x24 0
 
 li x22,0
 li x23,16
@@ -16,12 +16,12 @@ bge x22,x23,END
 sub x5,x5,x7
 bltz x5,L2b 	# branch less than zero
 L2a:
-slli x20,x20,1	# shift left logical immediate
-ori x20,x20,1	# or immediate
+slli x24,x24,1	# shift left logical immediate
+ori x24,x24,1	# or immediate
 j L3
 L2b:
 add x5,x5,x7
-slli x20,x20,1
+slli x24,x24,1
 L3:
 srai x7,x7,1	# shift rigth arithmetic immediate
 
